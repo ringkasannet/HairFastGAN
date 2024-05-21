70a3a7e933547e5afa3da5e4c2d4ec3fb946685a
ringkasan.net <ringkasan.net@gmail.com>
2024-05-21 20:58:25 +0700

    add start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+source ~/.profile
+conda  create -n hairfast python=3.10 -y
+conda activate hairfast
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs
+apt update
+apt install zip -y
+wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+unzip ninja-linux.zip -d /usr/local/bin/
+update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
+
+git clone https://huggingface.co/AIRI-Institute/HairFastGAN
+cd HairFastGAN && git lfs pull && cd ..
+mv HairFastGAN/pretrained_models pretrained_models
+mv HairFastGAN/input input
+rm -rf HairFastGAN
+pip install -r requirements.txt
