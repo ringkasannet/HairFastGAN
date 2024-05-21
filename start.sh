fc4737021498165874fb8629467b0fd8eae686a5
ringkasan.net <ringkasan.net@gmail.com>
2024-05-21 21:00:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+ #!/bin/bash 
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
