cf9305a4255da16e6bcf8c50a31517ed806fbfaa
ringkasan.net <ringkasan.net@gmail.com>
2024-05-22 13:33:58 +0700

    download model

--- start.sh
+++ start.sh
@@ -24,3 +24,8 @@ wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.z
 unzip ninja-linux.zip -d /usr/local/bin/
 update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
 
+git clone https://huggingface.co/AIRI-Institute/HairFastGAN
+cd HairFastGAN && git lfs pull && cd ..
+mv HairFastGAN/pretrained_models pretrained_models
+mv HairFastGAN/input input
+rm -rf HairFastGAN
