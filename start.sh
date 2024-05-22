b1991a28c010ff45737918886fdd3475e7f67c9f
ringkasan.net <ringkasan.net@gmail.com>
2024-05-22 13:32:04 +0700

    install conda

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ apt install zip -y
 apt install wget
 apt install git
 apt-get install -y git
-# mkdir -p ~/miniconda3
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-# rm -rf ~/miniconda3/miniconda.sh
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
 source ~/.profile
