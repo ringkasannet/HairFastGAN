63063f3a157d47830b8293f1102bf5e6914a666e
ringkasan.net <ringkasan.net@gmail.com>
2024-05-26 17:25:39 +0700

    f

--- created.sh
+++ created.sh
@@ -11,9 +11,9 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
-/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
 /workspace/miniconda3/bin/conda init bash
 /workspace/miniconda3/bin/conda init zsh
+/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
 source ~/.profile
 
 /workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
