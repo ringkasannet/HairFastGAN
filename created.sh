e81232a041af911e59c7067c648ff2fa56eb1e4e
ringkasan.net <ringkasan.net@gmail.com>
2024-05-26 17:58:41 +0700

    f

--- created.sh
+++ created.sh
@@ -9,15 +9,17 @@ apt-get install git-lfs
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-
 /workspace/miniconda3/bin/conda init bash
 /workspace/miniconda3/bin/conda init zsh
-/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
-source ~/.profile
+source /workspace/miniconda3/etc/profile.d/conda.sh 
+
+rm -rf ~/miniconda3/miniconda.sh
 
 /workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
-/workspace/miniconda3/bin/activate hairfast
+source /workspace/miniconda3/etc/profile.d/conda.sh 
+conda activate hairfast 
+source ~/.profile
+
 /workspace/miniconda3/bin/conda install ipykernel
 ipython kernel install --user --name=hairfast
 
