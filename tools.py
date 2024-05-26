a7726ca96218bfdb5dec3ad8d8d621888f1d29ca
ringkasan.net <ringkasan.net@gmail.com>
2024-05-26 22:35:13 +0700

    f

--- tools.py
+++ tools.py
@@ -4,7 +4,67 @@ from PIL import Image
 import numpy as np
 from utils.shape_predictor import align_face
 from torchvision.utils import save_image
+import requests
+import os
+from google.cloud import storage
+import re
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="barberfits-visualizer-8410bd16f96c.json"
 
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The contents to upload to the file
+    # contents = "these are my contents"
+
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} uploaded to {bucket_name}."
+    )
+
+
+def upload_file(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def url_to_aligned_tensor(url):
+    tensor_image=image_to_tensors(url)
+    aligned=align_face(tensor_image,is_filepath=False, return_tensors=True)
+    return aligned
+    
 
 def image_to_tensors(url):
   """Reads an image from a URL and converts it to a list of PyTorch tensors.
@@ -35,21 +95,30 @@ def image_to_tensors(url):
   # Return the tensor as a list (for convenience)
   return [tensor] 
 
-url="https://metro.co.uk/wp-content/uploads/2020/12/Alex-Cooper-3e3f.jpg"
-tensor_image=image_to_tensors(url)
-aligned=align_face(tensor_image,is_filepath=False, return_tensors=True)
-output="/workspace/HairFastGAN/aligned/"
-save_image(aligned,f'{output}fromTensor.png')
+# url="https://metro.co.uk/wp-content/uploads/2020/12/Alex-Cooper-3e3f.jpg"
+# tensor_image=image_to_tensors(url)
+# aligned=align_face(tensor_image,is_filepath=False, return_tensors=True)
+# output="/workspace/HairFastGAN/aligned/"
+# save_image(aligned,f'{output}fromTensor.png')
 
 
-from utils.shape_predictor import align_face
-from torchvision.utils import save_image
+def get_file_name_from_url(url):
+    match = re.search(r'/([^/]+)\.(png|jpg)$', url)
+    if match:
+      return match.group(1)
+    else:
+        return None
 
-source="unaligned"
-output="/workspace/HairFastGAN/aligned/"
-for filename in os.listdir(source):
-    full_filename=os.path.join(source, filename)
-    if os.path.isfile(full_filename):
-        base, ext = os.path.splitext(filename)
-        aligned=align_face(full_filename,is_filepath=True, return_tensors=True)
-        save_image(aligned,f'{output}{base}.png')
+# print(get_file_name_from_url(url))
+
+# from utils.shape_predictor import align_face
+# from torchvision.utils import save_image
+
+# source="unaligned"
+# output="/workspace/HairFastGAN/aligned/"
+# for filename in os.listdir(source):
+#     full_filename=os.path.join(source, filename)
+#     if os.path.isfile(full_filename):
+#         base, ext = os.path.splitext(filename)
+#         aligned=align_face(full_filename,is_filepath=True, return_tensors=True)
+#         save_image(aligned,f'{output}{base}.png')
