dde5978ddd31cd9cd01174f81cbcdea2767c80c7
ringkasan.net <ringkasan.net@gmail.com>
2024-05-26 12:18:22 +0700

    f

--- created.sh
+++ created.sh
@@ -1,8 +1,24 @@
- !/bin/bash 
+!/bin/bash 
 rm /bin/sh && ln -s /bin/bash /bin/sh
-RUN conda  create -n hairfast python=3.10 -y
-RUN source activate hairfast
-RUN conda activate hairfast
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
+/workspace/miniconda3/bin/conda init bash
+/workspace/miniconda3/bin/conda init zsh
+source ~/.profile
+
+/workspace/miniconda3/bin/conda activate hairfast
+
+
+/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
+/workspace/miniconda3/bin/conda activate hairfast
+/workspace/miniconda3/bin/conda install ipykernel
+ipython kernel install --user --name=hairfast
+
 
 apt-get update
 apt-get install zip -y
--- setconda.sh
+++ setconda.sh
@@ -1,14 +1,14 @@
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+bash ~/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
-~/miniconda3/bin/conda  create -n hairfast python=3.10 -y
-~/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init zsh
+/workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
+/workspace/miniconda3/bin/conda init bash
+/workspace/miniconda3/bin/conda init zsh
 source ~/.profile
 
-~/miniconda3/bin/conda activate hairfast
+/workspace/miniconda3/bin/conda activate hairfast
 
 conda install ipykernel
 ipython kernel install --user --name=hairfast
