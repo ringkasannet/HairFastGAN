0c481901d45dbf949c146ab10574ca4cc993afb0
ringkasan.net <ringkasan.net@gmail.com>
2024-05-26 17:25:02 +0700

    f

--- created.sh
+++ created.sh
@@ -12,9 +12,9 @@ bash ~/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
 /workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
-# /workspace/miniconda3/bin/conda init bash
-# /workspace/miniconda3/bin/conda init zsh
-# source ~/.profile
+/workspace/miniconda3/bin/conda init bash
+/workspace/miniconda3/bin/conda init zsh
+source ~/.profile
 
 /workspace/miniconda3/bin/conda  create -n hairfast python=3.10 -y
 /workspace/miniconda3/bin/activate hairfast
